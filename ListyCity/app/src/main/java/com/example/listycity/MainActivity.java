ca86303b8475a68f6be91570d9f58ab7bb9a125a
Atonize <aamalia@ualberta.ca>
2026-01-16 13:56:52 -0700

    participation exercise

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.listycity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,10 +19,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static MainActivity Instance;
-    ListView cityList;
-    ArrayAdapter<String> cityAdapter;
-    ArrayList<City> cities;
-    ArrayList<String> namesList;
+    private ListView cityList;
+    private City selectedCity;
+    private LinearLayout promptArea;
+    private EditText inputArea;
+    private ArrayAdapter<String> cityAdapter;
+    private ArrayList<City> cities;
+    private ArrayList<String> namesList;
 
     public static MainActivity getInstance() {
         return Instance;
@@ -36,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         Instance = this;
+
+        promptArea = findViewById(R.id.prompt_name);
+        showPrompt(false);
 
         cityList = findViewById(R.id.city_list);
 
@@ -55,27 +64,70 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addCity("City");
+                showPrompt(true);
+            }
+        });
+
+        Button deleteButton = findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedCity != null && cities.contains(selectedCity))
+                {
+                    cities.remove(selectedCity);
+                    namesList.remove(selectedCity.getName());
+                    updateCityList();
+                }
+            }
+        });
+        Button confirmButton = findViewById(R.id.confirm_button);
+        inputArea = findViewById(R.id.input_area);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCity(inputArea.getText().toString());
+                showPrompt(false);
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                parent.setSelection((int)id);
+                selectedCity = cities.get((int)id);
             }
         });
     }
 
+    private void showPrompt(boolean show)
+    {
+        if (show) {
+            inputArea.setText("");
+            promptArea.setVisibility(View.VISIBLE);
+        }
+        else {
+            promptArea.setVisibility(View.GONE);
+        }
+    }
+
+    private void updateCityList()
+    {
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, namesList);
+        cityList.setAdapter(cityAdapter);
+    }
     public void addCity(String cityName)
     {
-        if (cities == null)
-        {
+        if (cities == null) {
             cities = new ArrayList<City>();
         }
 
-        if (namesList == null)
-        {
+        if (namesList == null) {
             namesList = new ArrayList<>();
         }
         City city = new City(cityName);
         cities.add(city);
         namesList.add(cityName);
-
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, namesList);
-        cityList.setAdapter(cityAdapter);
+        updateCityList();
     }
 }
